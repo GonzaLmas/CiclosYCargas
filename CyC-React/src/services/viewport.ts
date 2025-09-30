b70b2db7b3fa7a7419bd8ceef34ffffbeecc4cdf
GonzaLmas <gonzalolamas09@gmail.com>
2025-09-30 16:23:54 -0300

    fix: viewports mobile

--- CyC-React/src/services/viewport.ts
+++ CyC-React/src/services/viewport.ts
@@ -1,6 +1,8 @@
 function setVhVar(): void {
   const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
+  const vw = window.innerWidth * 0.01;
+  document.documentElement.style.setProperty("--vw", `${vw}px`);
 }
 
 function onResize(): void {
